bc986ce2dec4144a35f7aae09247a5b4170b0cd4
Yu-Ren Zhang <e8035669@gmail.com>
2023-04-29 10:20:02 +0800

    no test

--- recipe/build.sh
+++ recipe/build.sh
@@ -37,7 +37,7 @@ fi
    --prefix="$PREFIX" \
 
 make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
-fi
+# if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+# make check
+# fi
 make install
